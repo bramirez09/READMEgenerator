54caf9fa4fa07643992ef49bf20d455d775da33e
brecec <brendaramirez@brendas-mbp.lan>
2023-06-13 16:30:29 -0500

    starterfiles and questions array

---
+++ index.js
@@ -0,0 +1,62 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+//const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the Title of your project?',
+        name: 'project title',
+      },
+      {
+        type: 'Input',
+        message: 'Describe your project',
+        name: 'project description',
+      },
+      {
+        type: 'input',
+        message: 'If required, enter installation instructions',
+        name: 'installation instruction',
+      },
+      {
+        type: 'input',
+        message: 'If available, enter usage information',
+        name: 'usage information',
+      },
+      {
+        type: 'input',
+        message: 'If available, enter contribution guidelines',
+        name: 'contribution guidelines',
+      },
+      {
+        type: 'input',
+        message: 'If available, enter test instructions',
+        name: 'test instructions',
+      },
+      {
+        type: 'list',
+        message: 'Choose a license for application',
+        name: 'lice choice',
+        choices: ['choice1', 'choice2', 'choice3'],
+      },
+      {
+        type: 'input',
+        message: 'Enter GitHub username',
+        name: 'Github username',
+      },
+      {
+        type: 'input',
+        message: 'Enter email address',
+        name: 'Github username',
+      },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

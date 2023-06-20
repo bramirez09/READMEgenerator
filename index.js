34f2eb756db6e45c305f0aef07584bcd6f8bcd3d
brecec <brendaramirez@brendas-mbp.lan>
2023-06-20 18:07:56 -0500

    fixed line 129 error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const inquirer = require('inquirer');
 function generateMarkdown(data) {
     return `# ${data.project_title}
 
-    ${renderLicense(license)}
+    ${renderLicense(data.license)}
 
     ## Description:
      ${data.project_description}
@@ -100,7 +100,7 @@ const renderLicense = (license) => {
     if (license == "MIT") {
       return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
     } else if (license == "Apache 2.0") {
-      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     } else if (license == "IBM") {
       return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
     } else if (license == "None") {
---
+++ utils/READMEsample.md
@@ -0,0 +1,38 @@
+# test
+
+    
+
+    ## Description:
+     test
+
+    
+    ## Table of Contents
+
+
+    ## Installation 
+     test
+
+
+    ## Usage:
+     test
+
+
+    ## Contribution: 
+     test
+
+
+    ## Tests:
+     test
+
+
+    ## License:
+     The product is license under IBM.
+     
+
+
+    ## Questions
+    For any questions, please contact me @ 
+    https://github.com/test
+    or
+    Email: test
+  

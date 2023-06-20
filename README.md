748c6ebc6d04c53fd83d4162a48a980c766fd217
brecec <brendaramirez@brendas-mbp.lan>
2023-06-20 17:32:24 -0500

    updated license function

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# test 9
-    ## undefined
-  
+# README Generator
+
+## Description
+
+The readme generator is a command-line application that accepts user input when prompted to create a readme.md file. The markdown file allows for user input for project description, a table of contents, installation instructions, usage details, license options with badges, contribution section and a question section. 
+
+## Visuals
+
+## Installation
+
+This command-line application contains node.js, inquirer package and the use of the operating system terminal.
+
+## Usage
+
+The mardown generator application can be used to create a high-quality, professional readme.md.  
   
--- index.js
+++ index.js
@@ -2,21 +2,39 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 //const generateMarkdown = require('./utils/generateMarkdown.js');
+
 function generateMarkdown(data) {
     return `# ${data.project_title}
+
+    ${renderLicense(license)}
+
     ## Description:
      ${data.project_description}
+
+    
     ## Table of Contents
+
+
     ## Installation 
      ${data.installation_instruction}
+
+
     ## Usage:
      ${data.usage_information}
+
+
     ## Contribution: 
      ${data.contribution_guidelines}
+
+
     ## Tests:
      ${data.test_instructions}
+
+
     ## License:
-     ${data.license_choice}
+     The product is license under ${data.license_choice}.
+     
+
 
     ## Questions
     For any questions, please contact me @ 
@@ -35,48 +53,62 @@ const questions = [
         message: 'What is the Title of your project?',
         name: 'project_title',
     },
-    // {
-    //     type: 'Input',
-    //     message: 'Describe your project',
-    //     name: 'project_description',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'If required, enter installation instructions',
-    //     name: 'installation_instruction',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'If available, enter usage information',
-    //     name: 'usage_information',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'If available, enter contribution guidelines',
-    //     name: 'contribution_guidelines',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'If available, enter test instructions',
-    //     name: 'test_instructions',
-    // },
-    // {
-    //     type: 'list',
-    //     message: 'Choose a license for application',
-    //     name: 'license_choice',
-    //     choices: ['Apache 2.0', 'IBM', 'ISC','MIT'],
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Enter GitHub username',
-    //     name: 'Github_username',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Enter email address',
-    //     name: 'email',
-    // },
+    {
+        type: 'Input',
+        message: 'Describe your project',
+        name: 'project_description',
+    },
+    {
+        type: 'input',
+        message: 'If required, enter installation instructions',
+        name: 'installation_instruction',
+    },
+    {
+        type: 'input',
+        message: 'If available, enter usage information',
+        name: 'usage_information',
+    },
+    {
+        type: 'input',
+        message: 'If available, enter contribution guidelines',
+        name: 'contribution_guidelines',
+    },
+    {
+        type: 'input',
+        message: 'If available, enter test instructions',
+        name: 'test_instructions',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for application',
+        name: 'license_choice',
+        choices: ['Apache 2.0', 'IBM', 'MIT'],
+    },
+    {
+        type: 'input',
+        message: 'Enter GitHub username',
+        name: 'Github_username',
+    },
+    {
+        type: 'input',
+        message: 'Enter email address',
+        name: 'email',
+    },
 ]
+// license function
+const renderLicense = (license) => {
+    if (license == "MIT") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license == "Apache 2.0") {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else if (license == "IBM") {
+      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+    } else if (license == "None") {
+      return "No Licenses were used.";
+    } else {
+      return "";
+    }
+  };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -97,7 +129,7 @@ function init() {
     .then ((answers) => {
         const contentForMd = generateMarkdown (answers);
         //console.log(contentForMd);
-        writeToFile("./utils/README.md",contentForMd);
+        writeToFile("./utils/READMEsample.md",contentForMd);
     })
 }
 //^ call inquire .prompt ().then
